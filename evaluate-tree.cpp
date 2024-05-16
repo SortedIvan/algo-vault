d1848a63f7ba2a29cce5d69a88dfdacd50200d5c
Ivan Ovcharov <62967263+SortedIvan@users.noreply.github.com>
2024-05-16 22:39:59 +0200

    Create evaluate-tree.cpp

---
+++ evaluate-tree.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool evaluateTree(TreeNode* root) 
+    {
+        if (!root)
+        {
+            return false;
+        }
+        return evaluate(root);
+    }
+
+    bool evaluate(TreeNode* root) 
+    {
+        if (!root->left && !root->right)
+        {
+            return root->val;
+        }
+
+        if (root->val == 2)
+        {
+            return evaluate(root->left) || evaluate(root->right);
+        }
+
+        return evaluate(root->left) && evaluate(root->right);
+    }
+};
