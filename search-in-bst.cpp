54e645604ed6a07ba8e95adfa62eab4a60c63907
Ivan Ovcharov <62967263+SortedIvan@users.noreply.github.com>
2024-05-27 10:15:43 +0200

    Create search-in-bst.cpp

---
+++ search-in-bst.cpp
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        return findSubtree(root, val);
+    }
+
+
+    TreeNode* findSubtree(TreeNode* root, int val)
+    {
+        if (!root)
+        {
+            return nullptr;
+        }
+
+        if (root->val == val)
+        {
+            return root;
+        }
+
+        if (root->val > val)
+        {
+            // go left of the tree
+            return findSubtree(root->left, val);
+        }
+
+        if (root->val < val)
+        {
+            return findSubtree(root->right, val);
+        }
+
+        return nullptr;
+    }
+
+};
