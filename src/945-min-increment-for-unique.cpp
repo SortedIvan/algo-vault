4261aa029a15e72fc8558df5745de20371c48e32
Ivan Ovcharov <62967263+SortedIvan@users.noreply.github.com>
2024-06-14 11:01:47 +0200

    Create 945-min-increment-for-unique.cpp

---
+++ src/945-min-increment-for-unique.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int minIncrementForUnique(vector<int>& nums) {
+
+        if (nums.size() == 0) {
+            return 0;
+        }
+
+        if (nums.size() == 1) {
+            return 1;
+        }
+
+        std::sort(nums.begin(), nums.end());
+
+        int amount = 0;
+
+        for (int i = 1; i < nums.size(); ++i) {
+            if (nums[i] == nums[])
+        }
+
+        return moves;
+    }
+};
